f8cf06b84f6a9dc1333525c6a19c80b3d6f96696
Gieron, Marcel <m.gieron@douglas.de>
2023-05-24 11:46:04 +0200

    ads.txt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="de">
+    <Html lang="de" className={process.env.WEBSITE}>
       <Head />
       <body style={{ backgroundImage: "url(" + process.env.BG_IMAGE + ")" }}>
         <Main />
